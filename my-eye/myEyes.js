a38a7565a4db5335e588f722da1b4a9f84634a6d
Felipe <felipegtx@users.noreply.github.com>
2016-02-16 14:16:42 -0200

    Update myEyes.js

--- my-eye/myEyes.js
+++ my-eye/myEyes.js
@@ -57,8 +57,8 @@
 
                                    _canvasPaletteContext.font = "25px Kaushan Script";
                                    _canvasPaletteContext.strokeStyle = "black";
-                                   _canvasPaletteContext.strokeText("Para os meus olhos", 470, 410);
-                                   _canvasPaletteContext.fillText("Para os meus olhos", 470, 410);
+                                   _canvasPaletteContext.strokeText("Para os meus olhos", 480, 415);
+                                   _canvasPaletteContext.fillText("Para os meus olhos", 480, 415);
 
                                     _canvasPaletteContext.font = "55px Kaushan Script";
                                    _canvasPaletteContext.strokeStyle = "black";
