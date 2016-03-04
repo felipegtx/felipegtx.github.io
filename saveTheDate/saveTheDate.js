7aad56fd1b6b1380a306d3e2c4bcb4512cbdf526
Felipe Teixeira <felipe.g.te@gmail.com>
2016-03-03 23:07:17 -0300

    More implementations

--- saveTheDate/saveTheDate.js
+++ saveTheDate/saveTheDate.js
@@ -1,4 +1,4 @@
-(function () {
+﻿(function () {
 
     "use strict";
 
@@ -7,17 +7,29 @@
             canvas = document.getElementById(idCanvas),
             windowH = $(window).height(),
             windowW = $(window).width(),
+            colors = {
+                red: "#FFAAAA",
+                white: "White",
+                green: "#7CBB91",
+                yellow: "#FFF8AA",
+                blue: "#6B949E",
+                black: "gray"
+            },
             canvasCtx = canvas.getContext("2d"),
             max = { x: 0, y: 0 },
+            writing = new buzz.sound("writing.ogg", { preload: true, loop: false, volume: 100, autoplay: false }),
+            erasing = new buzz.sound("eraser.ogg", { preload: true, loop: false, volume: 100, autoplay: false }),
+
             getRandomArbitrary = function (min, max) {
                 /// https://developer.mozilla.org/en/Core_JavaScript_1.5_Reference/Global_Objects/Math/random
                 return Math.random() * (max - min) + min;
             },
             clear = function (whenDone) {
                 var iteraction = 0,
-                    maxIteractions = 150;
-
+                    maxIteractions = 200;
+                erasing.setTime(2);
                 (function erase() {
+                    erasing.play();
                     canvasCtx.globalCompositeOperation = "destination-out";
                     canvasCtx.beginPath();
                     canvasCtx.strokeStyle = "black";
@@ -30,6 +42,7 @@
                     if (++iteraction < maxIteractions) {
                         requestAnimationFrame(erase);
                     } else {
+                        erasing.stop();
                         if (typeof whenDone === "function") {
                             whenDone.call();
                         }
@@ -49,7 +62,7 @@
                         "FontAwesome"],
                     dashLen = 100,
                     dashOffset = dashLen,
-                    speed = options.speed || 5,
+                    speed = options.speed || 100,
                     x = options.x || 10,
                     y = options.y || 90,
                     i = 0,
@@ -64,10 +77,15 @@
                 canvasCtx.font = fontSize + " " + font;
                 canvasCtx.lineWidth = 1;
                 canvasCtx.lineJoin = "round";
-                canvasCtx.globalAlpha = options.alpha || 0.5;
-                canvasCtx.fillStyle = options.fillStyle || "silver";
-                canvasCtx.strokeStyle = options.strokeStyle || "white";
+                canvasCtx.globalAlpha = options.alpha || 0.9;
+
+                if (options.fillStyle) {
+                    canvasCtx.fillStyle = options.fillStyle;
+                }
+                canvasCtx.strokeStyle = options.strokeStyle || colors.white;
+                writing.setTime(3);
                 (function loop() {
+                    writing.play();
                     var textM = canvasCtx.measureText(txt[i]);
                     canvasCtx.clearRect(x, 0, textM.width, textM.height);
                     canvasCtx.setLineDash([dashLen - dashOffset, dashOffset - speed]);
@@ -77,7 +95,9 @@
                     if (dashOffset > 0) {
                         requestAnimationFrame(loop);
                     } else {
-                        canvasCtx.fillText(txt[i], x, y);
+                        if (options.fillStyle) {
+                            canvasCtx.fillText(txt[i], x, y);
+                        }
                         dashOffset = dashLen;
                         x += canvasCtx.measureText(txt[i++]).width + canvasCtx.lineWidth * Math.random();
                         canvasCtx.setTransform(1, 0, 0, 1, 0, 3 * Math.random());
@@ -89,22 +109,54 @@
                             max.x = Math.max(max.x, textMeasurement.width + (parseInt(fontSize) * txt.length));
                             max.y = Math.max(max.y, (parseInt(fontSize) * txt.length));
                             if (typeof whenDone === "function") {
+                                writing.stop();
                                 whenDone.call();
                             }
                         }
                     }
                 })();
             };
+
+        if (!buzz.isSupported()) {
+            console.warn("Old browser is old.")
+        }
+
+        //writeText("teste0", 0, { y: 480, x: 500 }, function () {
+        //    writeText("teste1", 1, { y: 60, x: 500}, function () {
+        //        writeText("teste2", 2, { y: 120, x: 500}, function () {
+        //            writeText("teste3", 3, { y: 180, x: 500}, function () {
+        //                writeText("teste4", 4, { y: 240, x: 500}, function(){
+        //                    writeText("teste5", 5, { y: 320, x: 500});
+        //                });
+        //            });
+        //        });
+        //    });
+        //});
+
         canvas.height = windowH - 20;
         canvas.width = windowW - 20;
         writeText("Amanda", 4, { x: 100, y: 100 },
             function () {
-                writeText("&", 5, { x: 400, y: 130, strokeStyle: "red", fillStyle: "red", fontSize: "115px" },
+                writeText("&", 5, { x: 400, y: 130, strokeStyle: colors.red, fontSize: "115px" },
                     function () {
                         writeText("Felipe", 4, { x: 450, y: 180 }, function () {
                             var timeout = window.setTimeout(function () {
                                 window.clearTimeout(timeout);
-                                clear();
+                                clear(function () {
+                                    writeText("Metade da vida", 3,
+                                        { x: 150, y: 130, strokeStyle: colors.red, fontSize: "130px" },
+                                        function () {
+                                            writeText("compartilhando", 5,
+                                                { x: 220, y: 215, fillStyle: colors.white, strokeStyle: colors.yellow, fontSize: "90px" },
+                                                function () {
+                                                    writeText("histórias", 3,
+                                                    { x: 185, y: 320, strokeStyle: colors.white, fontSize: "135px" },
+                                                    function () {
+
+                                                    });
+                                                });
+                                        });
+                                });
                             }, 1000);
                         });
                     });
